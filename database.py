a9c4b258187e77e93ad95e67e97e0def5a4b0285
Tal <89003454+Talolacat123@users.noreply.github.com>
2021-11-01 19:15:59 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import *
+from textblob import TextBlob
 
 engine = create_engine('sqlite:///database.db?check_same_thread=False')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# ------------Functions---------------
-#def add_user(username, email, bio):
-#	user_obj = User(
-#		username=username,
-#		email=email,
-#		bio=bio)
-#	session.add(user_obj)
-#	session.commit()
-#	print("added " + username)
-
 def add_album(sub_by, album_name, artist, image_link, about, why_important, stafforstudent):
+	sen = TextBlob(why_important)
+	polarity = sen.sentiment.polarity;
 	album_obj = Album(
 		album_name=album_name,
 		artist = artist,
@@ -25,10 +18,10 @@ def add_album(sub_by, album_name, artist, image_link, about, why_important, staf
 		about=about,
 		why_important=why_important,
 		sub_by = sub_by,
-		stafforstudent = stafforstudent)
+		stafforstudent = stafforstudent,
+		sen = polarity)
 	session.add(album_obj)
 	session.commit()
-#	print(image_link + album_name +"by "+ album_name + "submitted by: " + sub_by + "about: " + about + "" + "Why its important to me: " + why_important)
 
 def query_by_type(stafforstudent):
    picks = session.query(
@@ -36,15 +29,5 @@ def query_by_type(stafforstudent):
        stafforstudent=stafforstudent).all()
    return picks
 
-#def query_all():
-#   """
-#   Print all the students
-#   in the database.
-#   """
-#   albums = session.query(
-#      Album).all()
-#   return albums
-
-#print (query_all())
 print(query_by_type("staff"))
 print(query_by_type("student"))
