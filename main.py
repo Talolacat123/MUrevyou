8895fe4baad38b49a33575949a7c2b73a02f5018
Tal <89003454+Talolacat123@users.noreply.github.com>
2021-11-01 19:13:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def submit():
 
 @app.route("/staffpicks")
 def staffpicks():
-    return render_template("staffpicks.html",albums = query_by_type("staff"), polarity = polarity, sen = sen)
+    return render_template("staffpicks.html",albums = query_by_type("staff"))
 
 @app.route("/studentpicks")
 def studentpicks():
-    return render_template("studentpicks.html", albums = query_by_type("student"), polarity = polarity, sen = sen)
+    return render_template("studentpicks.html", albums = query_by_type("student"))
 
 @app.route("/form", methods = ['GET','POST'])
 def homepage():
